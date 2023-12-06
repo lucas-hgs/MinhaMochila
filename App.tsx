1e950aa85112edbdb1fc35dab58e07cacc519a7f
Lucas Henrique Gonçalves da Silveira <lucas.hgs03@gmail.com>
2023-12-06 00:31:38 -0300

    feat: Adicionado Shopify e tema para aplicação.
    Adicionado a primeira parte do tema da aplicação com as cores.

--- App.tsx
+++ App.tsx
@@ -1,15 +1,17 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
+import {SafeAreaView} from 'react-native';
+import {ThemeProvider} from '@shopify/restyle/dist/context';
 import {Text} from './src/components/Text/Text';
+import {theme} from './src/theme/theme';
 
 function App(): JSX.Element {
-  return <Text preset="headingLarge">Teste</Text>;
+  return (
+    <ThemeProvider theme={theme}>
+      <SafeAreaView>
+        <Text preset="headingLarge">Test</Text>
+      </SafeAreaView>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/theme/theme.ts
@@ -0,0 +1,46 @@
+import {createTheme} from '@shopify/restyle';
+
+const palette = {
+  lightOcean: '#0FC2C0',
+  ocean: '#0CABA8',
+  greenOcean: '#008F8C',
+  darkOcean: '#015958',
+  darkerOcean: '#023535',
+
+  carrotSecondary: '#F86F2D',
+  carrotSecondaryLight: '#FAE6DD',
+
+  greenSuccess: '#4ABC86',
+  greenSuccessLight: '#D8FFEC',
+
+  redError: '#EA3838',
+  redErrorLight: '#FBECEC',
+
+  grayBlack: '#000000',
+  gray1: '#636363',
+  gray2: '#8E8E8E',
+  gray3: '#B3B3B3',
+  gray4: '#E1E1E1',
+  gray5: '#F5F5F5',
+  grayWhite: '#FFFFFF',
+};
+
+export const theme = createTheme({
+  colors: {
+    ...palette,
+    primary: palette.grayWhite,
+
+    buttonPrimary: palette.carrotSecondary,
+
+    background: palette.darkerOcean,
+
+    error: palette.redError,
+    errorLight: palette.redErrorLight,
+
+    success: palette.greenSuccess,
+    successLight: palette.greenSuccessLight,
+  },
+  spacing: {},
+});
+
+export type Theme = typeof theme;
