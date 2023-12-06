244030ca7463534dc6fd785794889cf67013ed5e
Lucas Henrique Gonçalves da Silveira <lucas.hgs03@gmail.com>
2023-12-06 01:46:55 -0300

    refactor: Refatorado o componente Button para utilizar o componente "TouchableOpacityBox" em sua estrutura.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,24 +1,38 @@
 import {useTheme} from '@shopify/restyle';
 import React from 'react';
-import {TouchableOpacity} from 'react-native';
+import {ActivityIndicator} from 'react-native';
 import {Theme} from '../../theme/theme';
+import {TouchableOpacityBox, TouchableOpacityBoxProps} from '../Box/Box';
 import {Text} from '../Text/Text';
 
-interface ButtonProps {
+interface ButtonProps extends TouchableOpacityBoxProps {
   title: string;
+  loading?: boolean;
 }
 
-export function Button({title}: ButtonProps) {
+export function Button({
+  title,
+  loading,
+  ...touchableOpacityBoxProps
+}: ButtonProps) {
   const {colors} = useTheme<Theme>();
 
   return (
-    <TouchableOpacity
+    <TouchableOpacityBox
+      height={50}
+      alignItems="center"
+      justifyContent="center"
       style={{
         backgroundColor: colors.backgroundDarkerOcean,
-      }}>
-      <Text preset="paragraphMedium" bold style={{color: colors.grayWhite}}>
-        {title}
-      </Text>
-    </TouchableOpacity>
+      }}
+      {...touchableOpacityBoxProps}>
+      {loading ? (
+        <ActivityIndicator />
+      ) : (
+        <Text preset="paragraphMedium" bold style={{color: colors.grayWhite}}>
+          {title}
+        </Text>
+      )}
+    </TouchableOpacityBox>
   );
 }
