3ef4888b1ec0ac85ee8b601e127a75b89c7676cf
Lucas Henrique Gonçalves da Silveira <lucas.hgs03@gmail.com>
2023-12-06 01:41:33 -0300

    feat: Adicionado o Touchable Opacity Box para ser exportado do componente Box.

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,4 +1,33 @@
-import {createBox} from '@shopify/restyle';
+import {
+  backgroundColor,
+  BackgroundColorProps,
+  border,
+  BorderProps,
+  createBox,
+  createRestyleComponent,
+  layout,
+  LayoutProps,
+  spacing,
+  SpacingProps,
+  spacingShorthand,
+  SpacingShorthandProps,
+} from '@shopify/restyle';
+import {TouchableOpacity, TouchableOpacityProps} from 'react-native';
 import {Theme} from '../../theme/theme';
 
 export const Box = createBox<Theme>();
+
+export type TouchableOpacityBoxProps = BackgroundColorProps<Theme> &
+  SpacingProps<Theme> &
+  LayoutProps<Theme> &
+  BorderProps<Theme> &
+  SpacingShorthandProps<Theme> &
+  TouchableOpacityProps;
+
+export const TouchableOpacityBox = createRestyleComponent<
+  TouchableOpacityBoxProps,
+  Theme
+>(
+  [backgroundColor, spacing, spacingShorthand, layout, border],
+  TouchableOpacity,
+);
