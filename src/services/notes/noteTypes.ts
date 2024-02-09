57b4f4df3bcc4b7598462676785dc8229a232cd2
Lucas Henrique Gonçalves da Silveira <lucas.hgs03@gmail.com>
2024-02-08 22:57:38 -0300

    fix: removed userId from noteTypes

--- src/services/notes/noteTypes.ts
+++ src/services/notes/noteTypes.ts
@@ -2,7 +2,6 @@ import {Note} from '@domain';
 
 export interface NotesService {
   note: Note | null;
-  userId: number | null;
   saveNote: (note: Note) => Promise<void>;
   removeNote: () => Promise<void>;
   isLoading: boolean;
