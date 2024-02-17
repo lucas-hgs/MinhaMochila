2876511f60d87ef723c0401ad75a0b9719a59044
Lucas Henrique Gonçalves da Silveira <lucas.hgs03@gmail.com>
2024-02-16 22:33:15 -0300

    feat: added delete activity function

--- src/screens/app/ActivityScreen/ActivityScreen.tsx
+++ src/screens/app/ActivityScreen/ActivityScreen.tsx
@@ -1,6 +1,13 @@
 import React, {useState} from 'react';
+import {Alert} from 'react-native';
 
-import {Activity, useActivityService} from '@services';
+import {useNavigation} from '@react-navigation/native';
+import {
+  Activity,
+  activityStorage,
+  useActivityService,
+  useToastService,
+} from '@services';
 
 import {Button, Screen} from '@components';
 import {AppScreenProps} from '@routes';
@@ -10,23 +17,69 @@ import {ReadActivity} from './components/ReadActivity';
 
 export function ActivityScreen({route}: AppScreenProps<'ActivityScreen'>) {
   const [edit, setEdit] = useState(false);
-  const {activity} = useActivityService();
+  const {activity, saveActivity} = useActivityService();
+  const {showToast} = useToastService();
+  const {navigate} = useNavigation();
 
   const actualActivity = activity?.filter(
     act => act.id === route.params.id,
   ) as Activity[];
+
+  function deleteConfirmation() {
+    Alert.alert(
+      'Você realmente quer deletar a atividade?',
+      'Após confirmar, sua atividade será deletada permanentemente!',
+      [
+        {
+          text: 'Cancelar',
+          style: 'cancel',
+        },
+        {
+          text: 'Confirmar',
+          onPress: deleteActivity,
+        },
+      ],
+    );
+  }
+
+  async function deleteActivity() {
+    try {
+      navigate('HomeScreen');
+
+      const updatedActivityList = activity?.filter(
+        item => item.id !== route.params.id,
+      ) as Activity[];
+
+      await activityStorage.set(updatedActivityList);
+      saveActivity(updatedActivityList);
+    } catch (error) {
+      showToast({
+        message:
+          'Não é possível deletar essa atividade agora, tente mais tarde!',
+        duration: 3000,
+      });
+    } finally {
+      showToast({message: 'Atividade deletada com sucesso!', duration: 3000});
+    }
+  }
 
   return (
     <Screen canGoBack>
       {edit === false ? (
         <>
           <ReadActivity actualActivity={actualActivity} />
+
           <Button
             title="Editar Atividade"
             mt="s48"
             onPress={() => setEdit(!edit)}
           />
-          <Button title="Deletar Atividade" mt="s24" preset="danger" />
+          <Button
+            title="Deletar Atividade"
+            mt="s24"
+            preset="danger"
+            onPress={deleteConfirmation}
+          />
         </>
       ) : (
         <>
