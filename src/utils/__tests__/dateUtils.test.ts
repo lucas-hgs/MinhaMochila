4fddf21475d92e042ec7d117d8a19df8ed64234a
Lucas Henrique Gonçalves da Silveira <lucas.hgs03@gmail.com>
2024-03-12 19:37:56 -0300

    test: tested dateUtils functions

---
+++ src/utils/__tests__/dateUtils.test.ts
@@ -0,0 +1,29 @@
+import {format} from 'date-fns';
+
+import {dateUtils} from '../dateUtils';
+
+const MOCKED_DATE = '1995-06-03';
+
+describe('dateUtils', () => {
+  describe('formatDate', () => {
+    it('should format date to dd/MM/yyyy', () => {
+      const timeFormatted = format(MOCKED_DATE, 'dd/MM/yyyy');
+
+      expect(dateUtils.formatDate(MOCKED_DATE).formatedDate).toBe(
+        timeFormatted,
+      );
+    });
+
+    it('should format time to HH:mm', () => {
+      const timeFormatted = format(MOCKED_DATE, 'HH:mm');
+
+      expect(dateUtils.formatDate(MOCKED_DATE).formatedTime).toBe(
+        timeFormatted,
+      );
+    });
+
+    it('should format date to yyyy-mm-dd format', () => {
+      expect(dateUtils.formatDateToISO('03/06/1995')).toBe('1995-06-03');
+    });
+  });
+});
