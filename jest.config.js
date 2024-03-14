590bd05d6a6c974df5a6436f2fc3a01519d0ab34
Lucas Henrique Gonçalves da Silveira <lucas.hgs03@gmail.com>
2024-03-13 22:44:44 -0300

    feat: added test mock for Async Storage

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   preset: 'react-native',
+  collectCoverageFrom: ['src/{components,utils}/**/*.{js,jsx,ts,tsx}'],
+  coveragePathIgnorePatterns: ['/node_modules', 'index'],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
 };
